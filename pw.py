7809a644f9b7e945dfe0de81dd88f0340f40d66e
Chanchal Alam <106960500+chanchalalam@users.noreply.github.com>
2024-09-11 20:21:17 +0530

    Update pw.py

--- pw.py
+++ pw.py
@@ -39,7 +39,15 @@ I've also leveraged RapidMiner and Informatica tools to streamline data workflow
 I'm working on MLOps, where I'm applying my skills to streamline the machine learning lifecycle, from model development to deployment and monitoring. 
 I'm excited to explore the latest advancements in MLOps and contribute to the development of more efficient and effective AI solutions.
 I thrive on leveraging these skills to drive meaningful impact and advance the field of data science and AI. With a passion for collaboration and innovation, 
-I'm always looking for new opportunities to apply my expertise and make a difference. 
+I'm always looking for new opportunities to apply my expertise and make a difference.
+Advanced-Resume-Tracking-System
+This project aims to optimize the recruitment process. It integrates an advanced Applicant Tracking System with Google Gemini Pro, streamlining resume parsing, keyword matching, and candidate evaluation for an efficient end-to-end solution in talent acquisition.
+Math With Gesture
+In this project, I created a system that can solve mathematical problems using hand gestures and computer vision techniques. By leveraging OpenCV and MediaPipe libraries, I developed a program that allows users to draw mathematical expressions on a surface, captures the gestures using a camera, and provides the solution with a step-by-step explanation.
+YouTube-Video-to-Detailed-Notes-Converter
+The YouTube Video to Detailed Notes Converter is an application that leverages the power of Generative AI, specifically the Gemini Pro model, to automatically generate detailed notes from YouTube video transcripts. This tool streamlines the process of extracting key insights from lengthy video content, making it an invaluable resource for students, researchers, and anyone seeking to efficiently consume video-based information.
+Multiple Disease Detection
+This project involves predicting multiple diseases such as Diabetes, Heart disease, Kidney disease, Breast cancer, Liver disease, Malaria, and Pneumonia using machine learning and deep learning algorithms. It includes a web application for predicting multiple diseases using these algorithms.
 
 Chanchal's Email: chanchalalam24@gmail.com 
 Chanchal's LinkedIn: https://www.linkedin.com/in/chanchalalam786/
