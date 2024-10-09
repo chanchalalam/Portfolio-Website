07089cedfc81e95c1c18b6f789b561d5dcefcc73
Chanchal Alam <106960500+chanchalalam@users.noreply.github.com>
2024-10-09 13:54:13 +0530

    Update pw.py

--- pw.py
+++ pw.py
@@ -2,7 +2,7 @@ import streamlit as st
 import google.generativeai as genai
 
 # Configure the generative AI model
-genai.configure(api_key="AIzaSyDGWVCY4Y6H_bLrpWVu_DuLoXgsh-_A6WI")
+genai.configure(api_key="AIzaSyBeXLsJtDU75icRNsnHBvrJsiESNW1y6BQ")
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 # Set the page configuration
